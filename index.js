722c23c4d83cc39a62febcadf5076d9ed8905a5e
Luka Talakvadze <lukatalakvadze@gmail.com>
2023-03-13 20:47:53 +0400

    added js part

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-"use strict";
+// "use strict";
 
-const loginButton = document.querySelector(".login-div");
-const emailInput = document.querySelector(".enter-email");
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// const loginButton = document.querySelector(".login-div");
+// const emailInput = document.querySelector(".enter-email");
+// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-loginButton.addEventListener("click", function () {
+// loginButton.addEventListener("click", function () {
+//   const email = emailInput.value;
+
+//   if (emailRegex.test(email)) {
+//     const user = {
+//       email: email,
+//     };
+//   } else {
+//     loginButton.style.backgroundColor = "red";
+//     loginButton.style.color = "white";
+//     loginButton.textContent = "Please check your email";
+//   }
+// });
+
+const emailInput = document.querySelector(".inputJs");
+const button = document.querySelector(".buttonJs");
+const error = document.querySelector(".error");
+const thanks = document.querySelector(".thanks");
+
+button.addEventListener("click", () => {
   const email = emailInput.value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const valid = emailRegex.test(email);
 
-  if (emailRegex.test(email)) {
-    const user = {
-      email: email,
-    };
+  if (valid) {
+    emailInput.style.borderColor = "green";
+    thanks.style.display = "block";
+    error.style.display = "none";
   } else {
-    loginButton.style.backgroundColor = "red";
-    loginButton.style.color = "white";
-    loginButton.textContent = "Please check your email";
+    emailInput.style.borderColor = "red";
+    error.style.display = "block";
+    thanks.style.display = "none";
   }
 });
